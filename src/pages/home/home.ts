36a89f2f2ece2cf0777a68bae966460b043b7cf0
ammar10 <ammar.tariq@hotmail.com>
2018-03-28 15:54:17 +0500

    final with bugs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,18 @@ import { DataServiceProvider } from '../../providers/data-service/data-service';
 })
 export class HomePage {
 
+ public itemsToDisplay;
 
+  inputValue:string;
 
   constructor(public navCtrl: NavController, public DS: DataServiceProvider) {
+    this.getItems()
     }
   
     
     setItem(inputValue){
+     
+      console.log(inputValue );
       if(inputValue){
       this.DS.setVal(inputValue)
       console.log(inputValue + ' is set in storage')
@@ -22,12 +27,20 @@ export class HomePage {
       }else{
         console.log('null values are not acceptable')
       }
+      this.inputValue = null
+      
     }
 
 
     getItems(){
-     let storageItems = this.DS.getVal();
-     console.log(storageItems)
+      let storageItems = null
+     storageItems = this.DS.getVal();
+     if(storageItems){
+     console.log("vals from storage", storageItems);
+    }else{
+      console.log('from else')
+      console.log(this.DS.getVal())
+    }
     }
 
  
--- src/providers/data-service/data-service.ts
+++ src/providers/data-service/data-service.ts
@@ -2,37 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-/*
-  Generated class for the DataServiceProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DataServiceProvider {
   storageArray = []
+  
   constructor(public http: HttpClient, public storage: Storage) {
     console.log('Hello DataServiceProvider Provider');
   }
 
 
 setVal(item){
-
   let items = []
   this.storageArray.push(item)
   items = this.storageArray
   this.storage.set('data',items)
-
  }
  
- getVal(){
+ async getVal(){
   let storageItems = []
   this.storage.get('data')
   .then((val)=>{
     storageItems = val
-    console.log(val)
+    console.log('storage items from provider',storageItems)
+    
+  }).catch((e)=>{
+    console.log(e)
   })
-  return storageItems
+  return await storageItems
  }
  
 
